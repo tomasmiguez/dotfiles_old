2417e2e93ea640fabda9ab4f00629487cddca09d
Tomas Miguez <tmiguez@fu.do>
2022-10-10 00:15:06 -0300

    tmp

--- .config/nvim/lua/tmiguez/config/telescope.lua
+++ .config/nvim/lua/tmiguez/config/telescope.lua
@@ -1,16 +1,33 @@
--- You dont need to set any of these options. These are the default ones. Only
--- the loading is important
+local actions = require('telescope.actions')
+
 require('telescope').setup {
+    defaults = {
+    mappings = {
+      i = {
+        ["<C-q>"] = actions.send_to_qflist,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      }
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--trim" -- add this value
+    }
+  },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
+      override_generic_sorter = true,
+      override_file_sorter = true,
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
     }
   }
 }
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
+
 require('telescope').load_extension('fzf')
