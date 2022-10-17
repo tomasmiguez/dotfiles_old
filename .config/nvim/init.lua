2d4992e314aa8045c968db8e59f7b5b8c1361b51
Tomas Miguez <tmiguez@fu.do>
2022-10-17 15:05:50 -0300

    feat: explicitly source autoload files
    They aren't getting sourced automatically for some reason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,5 @@
 require("tmiguez")
+
+-- Files not getting autoloaded for some reason
+vim.cmd [[ source ~/.config/nvim/autoload/SortGroup.vim ]]
+vim.cmd [[ source ~/.config/nvim/autoload/camelcasemotion.vim ]]
