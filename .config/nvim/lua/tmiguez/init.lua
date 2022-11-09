f77fc6ebee649b69e989022c7c9372d523944f77
Tomas Miguez <tmiguez@fu.do>
2022-11-09 15:20:53 -0300

    feat: yaml syntax highlighting without lsp diagnostics
    All yaml files are extremely broken for Rubocop, we can't activate
    diagnostics without making everything red.

--- .config/nvim/lua/tmiguez/init.lua
+++ .config/nvim/lua/tmiguez/init.lua
@@ -9,3 +9,14 @@ require("tmiguez.set")
 
 -- Plugins configuration
 require("tmiguez.config")
+
+-- Filetype detection
+vim.api.nvim_create_autocmd(
+    { "BufRead", "BufNewFile" },
+    { pattern = { "*.yajl" }, command = "setfiletype ruby" }
+)
+
+vim.api.nvim_create_autocmd(
+    { "BufRead", "BufNewFile" },
+    { pattern = { "*.yajl" }, command = "LspStop<cr>" }
+)
